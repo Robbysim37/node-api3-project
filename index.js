baa512b5a819fe94528c0dc8651de961b70a47ac
Robby Lewis <Robbysim37@gmail.com>
2023-01-16 19:17:13 -0500

    halfway Done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it
+const server = require("./api/server")
+const PORT = 8000
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+})
